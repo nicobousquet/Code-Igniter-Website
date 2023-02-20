658223369bc7736f328aac7a908db0cd66338674
nicobousquet <nbousquet99@gmail.com>
2023-02-20 01:05:41 +0100

    do not display price before selecting size AND quantity

--- CodeIgniterFolder/application/views/posterdescription.php
+++ CodeIgniterFolder/application/views/posterdescription.php
@@ -113,7 +113,7 @@
 	<script>
 		display_price = function () {
 			let size = $("input:radio[name ='radio_buttons']:checked").val();
-			let price = 1000;
+			let price = 0;
 
 			switch (size) {
 				case "40x50":
@@ -132,7 +132,10 @@
 					price = 99.99;
 					break;
 			}
-			document.getElementById('price').innerHTML = (parseInt(document.getElementById('quantity').value) * price).toFixed(2) + '$';
+
+			if (price != 0) {
+				document.getElementById('price').innerHTML = (parseInt(document.getElementById('quantity').value) * price).toFixed(2) + '$';
+			}
 		}
 	</script>
 </head>
@@ -149,25 +152,30 @@
 		<form class="size_form" name="size_form" action="<?= site_url('MyCart/add_to_cart/') . $photo[0]->id; ?>"
 			  method="post">
 			<div class="font title">
-				<?= $photo[0]->continent; ?>	#<?= $photo[0]->id; ?>
+				<?= $photo[0]->continent; ?> #<?= $photo[0]->id; ?>
 			</div>
 			<label class="font">Size (cm):</label>
 			<div class="size_container">
 				<!-- Radio buttons for selecting size of poster -->
 				<!-- 40x50 cm size option -->
-				<input id="size1" class="btn btn-default" type="radio" value="40x50" name="radio_buttons" onclick="display_price();" required>
+				<input id="size1" class="btn btn-default" type="radio" value="40x50" name="radio_buttons"
+					   onclick="display_price();" required>
 				<label for="size1">40 x 50</label>
 				<!-- 50x70 cm size option -->
-				<input id="size2" class="btn btn-default" type="radio" value="50x70" name="radio_buttons" onclick="display_price();">
+				<input id="size2" class="btn btn-default" type="radio" value="50x70" name="radio_buttons"
+					   onclick="display_price();">
 				<label for="size2">50 x 70</label>
 				<!-- 60x90 cm size option -->
-				<input id="size3" class="btn btn-default" type="radio" value="60x90" name="radio_buttons" onclick="display_price();">
+				<input id="size3" class="btn btn-default" type="radio" value="60x90" name="radio_buttons"
+					   onclick="display_price();">
 				<label for="size3">60 x 90</label>
 				<!-- 100x150 cm size option -->
-				<input id="size4" class="btn btn-default" type="radio" value="100x150" name="radio_buttons" onclick="display_price();">
+				<input id="size4" class="btn btn-default" type="radio" value="100x150" name="radio_buttons"
+					   onclick="display_price();">
 				<label for="size4">100 x 150</label>
 				<!-- 120x180 cm size option -->
-				<input id="size5" class="btn btn-default" type="radio" value="120x180" name="radio_buttons" onclick="display_price();">
+				<input id="size5" class="btn btn-default" type="radio" value="120x180" name="radio_buttons"
+					   onclick="display_price();">
 				<label for="size5">120 x 180</label>
 			</div>
 
